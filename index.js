db4389144a0ac39822f56308d23096252f70c18f
kali-octavio1504 <kali.octavio@gmail.com>
2025-02-23 15:02:10 -0600

    Se inicia servidor http

--- index.js
+++ index.js
@@ -4,5 +4,16 @@ const express = require('express');
 const cors = require('cors');
 
 console.log("app node iniciada");
-
 dbConect();
+
+const app = express();
+
+const port = process.env.PORT || 4100
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.listen(port, () => {
+    console.log("app corriendo dentro del puerto: http://localhost:" + port);
+})
