e1a1401b145880b875a850cb9e629d1c9fd91d71
edgar <seo@disoin.com>
2025-02-26 10:22:01 -0600

    Se crea el modelo del tanque

---
+++ models/Tanques.js
@@ -0,0 +1,43 @@
+const {Schema, model} = require('mongoose');
+
+const TanqueSchema = Schema({
+    Nombre: {
+        type: String,
+        required: true
+    },
+    Marca: {
+        type: String,
+        required: true
+    },
+    Capacidad: {
+        type: String,
+        required: true
+    },
+    Color: {
+        type: String,
+        required: true
+    },
+    Reforzamiento: {
+        type: Number,
+        required: true
+    },
+    Precio: {
+        type: Boolean,
+        required: true
+    },
+    PrecioOferta: {
+        type: Boolean,
+        default: 0
+    },
+    Fecha: {
+        type: Date,
+        default: Date.now
+    },
+    Imagen: {
+        type: String,
+        default: "default.png"
+    }
+
+});
+
+module.exports = model("Tanques", TanqueSchema, "tanques");
